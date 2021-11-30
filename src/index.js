f7fc2dc515ecae37fd16f5f73ce4c0b7f5e97ba8
Jose Ortiz <proyectosjgot@gmail.com>
2021-11-30 16:19:38 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log('hola mundo de nuevo cambie')
+
+function gol() {
+    console.log(`hola ${2}`)
+}
+// const f = false;
+const algo = (x) => console.log(x)
+const ASA = 'sa'
+console.log(ASA)
+
+algo()
